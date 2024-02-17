5c1e19f0eb56b67bd4f65e19148314a7cea2b040
DimasKama <mynameisiphone10@gmail.com>
2024-02-17 22:25:12 +0500

    0.1.2 - Fix translucent layers incorrectly display

---
+++ src/main/java/com/dimaskama/orthocamera/mixin/BufferBuilderMixin.java
@@ -0,0 +1,37 @@
+package com.dimaskama.orthocamera.mixin;
+
+import com.dimaskama.orthocamera.client.OrthoCamera;
+import com.google.common.primitives.Floats;
+import it.unimi.dsi.fastutil.ints.IntComparator;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.util.math.Vec3f;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(BufferBuilder.class)
+public abstract class BufferBuilderMixin {
+    @Shadow @Nullable private Vec3f[] sortingPrimitiveCenters;
+
+    @ModifyArg(
+            method = "writeSortedIndices",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lit/unimi/dsi/fastutil/ints/IntArrays;mergeSort([ILit/unimi/dsi/fastutil/ints/IntComparator;)V"
+            ),
+            index = 1
+    )
+    // Vertex sorting by Z
+    private IntComparator afterChange(IntComparator comp) {
+        if (OrthoCamera.isEnabled()) {
+            float[] fs = new float[sortingPrimitiveCenters.length];
+            for (int i = 0; i < fs.length; i++) {
+                fs[i] = -sortingPrimitiveCenters[i].getZ();
+            }
+            return (a, b) -> Floats.compare(fs[b], fs[a]);
+        }
+        return comp;
+    }
+}
