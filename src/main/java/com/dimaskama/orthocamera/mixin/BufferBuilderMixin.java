49698f0620a4ab6262d43491ce25bb1874a86601
DimasKama <mynameisiphone10@gmail.com>
2024-02-26 20:35:20 +0500

    0.1.4 - Fix compatibility issue with Sodium - Fix compatibility issue with CameraUtils - Fix camera shake on pressing camera fixing key

--- src/main/java/com/dimaskama/orthocamera/mixin/BufferBuilderMixin.java
+++ src/main/java/com/dimaskama/orthocamera/mixin/BufferBuilderMixin.java
@@ -2,36 +2,108 @@ package com.dimaskama.orthocamera.mixin;
 
 import com.dimaskama.orthocamera.client.OrthoCamera;
 import com.google.common.primitives.Floats;
-import it.unimi.dsi.fastutil.ints.IntComparator;
+import it.unimi.dsi.fastutil.ints.IntArrays;
 import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.VertexFormat;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
+import org.lwjgl.system.MemoryUtil;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(BufferBuilder.class)
+import java.nio.ByteBuffer;
+
+// Higher priority than Sodium
+@Mixin(value = BufferBuilder.class, priority = 1050)
 public abstract class BufferBuilderMixin {
     @Shadow @Nullable private Vector3f[] sortingPrimitiveCenters;
+    @Shadow private float sortingCameraX;
+    @Shadow private float sortingCameraY;
+    @Shadow private float sortingCameraZ;
+    @Shadow private int elementOffset;
+    @Shadow private ByteBuffer buffer;
 
-    @ModifyArg(
-            method = "writeSortedIndices",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lit/unimi/dsi/fastutil/ints/IntArrays;mergeSort([ILit/unimi/dsi/fastutil/ints/IntComparator;)V"
-            ),
-            index = 1
-    )
-    // Vertex sorting by Z
-    private IntComparator afterChange(IntComparator comp) {
-        if (OrthoCamera.isEnabled()) {
-            float[] fs = new float[sortingPrimitiveCenters.length];
-            for (int i = 0; i < fs.length; i++) {
-                fs[i] = -sortingPrimitiveCenters[i].z();
+    /**
+     * @author JellySquid
+     * @reason Copied & Modified Sodium optimization. Made for sorting indices by Z instead by the distance to camera, when Orthocamera is enabled. Needed to overwrite all of these for Sodium compatibility
+     */
+    @Overwrite
+    private void writeSortedIndices(VertexFormat.IndexType indexType) {
+        float[] distance = new float[this.sortingPrimitiveCenters.length];
+        int[] indices = new int[this.sortingPrimitiveCenters.length];
+
+        this.calculatePrimitiveDistances(distance, indices);
+
+        IntArrays.mergeSort(indices, (a, b) -> Floats.compare(distance[b], distance[a]));
+
+        this.writePrimitiveIndices(indexType, indices);
+    }
+
+    @Unique
+    private void calculatePrimitiveDistances(float[] distance, int[] indices) {
+        int i = 0;
+
+        while (i < this.sortingPrimitiveCenters.length) {
+            Vector3f pos = this.sortingPrimitiveCenters[i];
+
+            if (pos == null) {
+                throw new NullPointerException("Primitive center is null");
             }
-            return (a, b) -> Floats.compare(fs[b], fs[a]);
+
+            if (!OrthoCamera.isEnabled()) {
+                float x = pos.x() - this.sortingCameraX;
+                float y = pos.y() - this.sortingCameraY;
+                float z = pos.z() - this.sortingCameraZ;
+
+                distance[i] = (x * x) + (y * y) + (z * z);
+            } else {
+                distance[i] = -pos.z();
+            }
+
+            indices[i] = i++;
         }
-        return comp;
+    }
+
+    @Unique
+    private static final int[] VERTEX_ORDER = new int[] { 0, 1, 2, 2, 3, 0 };
+
+    @Unique
+    private void writePrimitiveIndices(VertexFormat.IndexType indexType, int[] indices) {
+        long ptr = MemoryUtil.memAddress(this.buffer, this.elementOffset);
+
+        switch (indexType) {
+            case BYTE -> {
+                for (int index : indices) {
+                    int start = index * 4;
+
+                    for (int offset : VERTEX_ORDER) {
+                        MemoryUtil.memPutByte(ptr, (byte) (start + offset));
+                        ptr += Byte.BYTES;
+                    }
+                }
+            }
+            case SHORT -> {
+                for (int index : indices) {
+                    int start = index * 4;
+
+                    for (int offset : VERTEX_ORDER) {
+                        MemoryUtil.memPutShort(ptr, (short) (start + offset));
+                        ptr += Short.BYTES;
+                    }
+                }
+            }
+            case INT -> {
+                for (int index : indices) {
+                    int start = index * 4;
+
+                    for (int offset : VERTEX_ORDER) {
+                        MemoryUtil.memPutInt(ptr, (start + offset));
+                        ptr += Integer.BYTES;
+                    }
+                }
+            }
+        }
     }
 }
