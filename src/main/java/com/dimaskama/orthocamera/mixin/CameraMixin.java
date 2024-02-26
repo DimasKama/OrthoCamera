86aa4f09cf1e718bfd8af2554d0dde21712056c3
DimasKama <mynameisiphone10@gmail.com>
2024-02-26 19:37:16 +0500

    0.1.3 - Fix compatibility issue with CameraUtils

--- src/main/java/com/dimaskama/orthocamera/mixin/CameraMixin.java
+++ src/main/java/com/dimaskama/orthocamera/mixin/CameraMixin.java
@@ -3,25 +3,19 @@ package com.dimaskama.orthocamera.mixin;
 import com.dimaskama.orthocamera.client.OrthoCamera;
 import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Camera.class)
 public abstract class CameraMixin {
-    @Shadow protected abstract void moveBy(double x, double y, double z);
 
-    @Redirect(
-            method = "update",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/Camera;moveBy(DDD)V",
-                    ordinal = 0
-            )
-    )
-    private void moveByInject(Camera instance, double x, double y, double z) {
-        if (!OrthoCamera.isEnabled()) {
-            moveBy(x, y, z);
-        }
+    @ModifyVariable(method = "moveBy", at = @At("HEAD"), index = 1, argsOnly = true)
+    private double moveByHeadX(double value) {
+        return OrthoCamera.isEnabled() ? 0.0 : value;
+    }
+
+    @ModifyVariable(method = "moveBy", at = @At("HEAD"), index = 3, argsOnly = true)
+    private double moveByHeadZ(double value) {
+        return OrthoCamera.isEnabled() ? 0.0 : value;
     }
 }
