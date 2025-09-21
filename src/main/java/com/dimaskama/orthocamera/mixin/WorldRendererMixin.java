7c34cf993771e40cb02a5845a73509dfc3dbd282
DimasKama <mynameisiphone10@gmail.com>
2025-09-21 17:13:42 +0500

    Fix backport

--- src/main/java/com/dimaskama/orthocamera/mixin/WorldRendererMixin.java
+++ src/main/java/com/dimaskama/orthocamera/mixin/WorldRendererMixin.java
@@ -1,23 +1,22 @@
 package com.dimaskama.orthocamera.mixin;
 
 import com.dimaskama.orthocamera.client.OrthoCamera;
-import net.minecraft.client.render.WorldBorderRendering;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.border.WorldBorder;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WorldBorderRendering.class)
-public class WorldBorderRenderingMixin {
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
 
     @Inject(
-            method = "render",
+            method = "renderWorldBorder",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void render(WorldBorder border, Vec3d vec3d, double d, double e, CallbackInfo ci) {
+    private void render(Camera camera, CallbackInfo ci) {
         if (!OrthoCamera.isEnabled() || !OrthoCamera.CONFIG.hide_world_border) {
             return;
         }
